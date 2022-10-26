3e64a18cd61c2928e947e4367c60f6bcf9b7ff74
PandasLikeBamboo <mredpanada@gmail.com>
2022-10-26 18:28:20 +0200

    added working permissions

--- src/main/java/me/dark/merchant/moderationcommandsgui/listeners/MainListener.java
+++ src/main/java/me/dark/merchant/moderationcommandsgui/listeners/MainListener.java
@@ -28,86 +28,95 @@ public class MainListener implements Listener {
         String name = p.getName();
 
 
+        if (p.hasPermission("mcg.menu")) {
+
+            if (e.getView().getTitle().equalsIgnoreCase(ChatColor.DARK_RED + "Main Menu")) {
 
 
-        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.DARK_RED + "Main Menu")) {
+                if (e.getCurrentItem() == null) {
+                    return;
+                }
+
+                switch (item) {
+                    case TOTEM_OF_UNDYING:
+                        if (p.hasPermission("mcg.menu.god")) {
+                            if (p.isInvulnerable()) {
+                                p.setInvulnerable(false);
+                                p.sendMessage(ChatColor.RED + "God off");
+                                e.getCurrentItem().removeEnchantment(Enchantment.MENDING);
+
+                                p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE, Note.natural(1, Note.Tone.G));
+                            } else {
+                                p.setInvulnerable(true);
+                                p.sendMessage(ChatColor.GREEN + "God on");
+                                e.getCurrentItem().addUnsafeEnchantment(Enchantment.MENDING, 1);
+
+                                p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE, Note.natural(1, Note.Tone.C));
+
+                            }
+                        } else {p.sendMessage(ChatColor.RED + "You don't have such power to execute this command (Required 'mcg.menu.god' permission)");}
+                        break;
+
+                    case FEATHER:
+                        if (p.hasPermission("mcg.menu.fly")) {
+                            if (p.getAllowFlight()) {
+                                p.setAllowFlight(false);
+                                p.sendMessage(ChatColor.RED + "Fly off");
+                                e.getCurrentItem().removeEnchantment(Enchantment.MENDING);
+
+                                p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE, Note.natural(1, Note.Tone.G));
+                            } else {
+                                p.setAllowFlight(true);
+                                p.sendMessage(ChatColor.GREEN + "Fly on");
+                                e.getCurrentItem().addUnsafeEnchantment(Enchantment.MENDING, 1);
+
+                                p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE, Note.natural(1, Note.Tone.C));
+                            }
+                        } else {p.sendMessage(ChatColor.RED + "You don't have such power to execute this command (Required 'mcg.menu.fly' permission)");}
+
+                        break;
+                    case GHAST_TEAR:
+                        if (p.hasPermission("mcg.menu.vanish")) {
+                            if (plugin.invList.contains(p)) {
+
+                                for (Player people : Bukkit.getOnlinePlayers()) {
+                                    people.showPlayer(plugin, p);
+                                }
+                                plugin.invList.remove(p);
+                                p.sendMessage(ChatColor.RED + "Vanish off");
+                                Bukkit.broadcastMessage(ChatColor.YELLOW + name + " joined the game");
+                                e.getCurrentItem().removeEnchantment(Enchantment.MENDING);
+
+                                p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE, Note.natural(1, Note.Tone.G));
+
+                                p.removeScoreboardTag("vanished");
 
 
-            if (e.getCurrentItem() == null) {
-                return;
+                            } else if (!plugin.invList.contains(p)) {
+
+                                for (Player people : Bukkit.getOnlinePlayers()) {
+                                    people.hidePlayer(plugin, p);
+                                }
+                                plugin.invList.add(p);
+                                p.sendMessage(ChatColor.GREEN + "Vanish on");
+                                Bukkit.broadcastMessage(ChatColor.YELLOW + name + " left the game");
+                                e.getCurrentItem().addUnsafeEnchantment(Enchantment.MENDING, 1);
+
+                                p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE, Note.natural(1, Note.Tone.C));
+
+                                p.addScoreboardTag("vanished");
+
+                            }
+                        } else {p.sendMessage(ChatColor.RED + "You don't have such power to execute this command (Required 'mcg.menu.vanish' permission)");}
+
+                        break;
+
+                    default:
+
+                }
+
+                e.setCancelled(true);
             }
-
-            switch (item){
-                case TOTEM_OF_UNDYING:
-                    if (p.isInvulnerable()){
-                        p.setInvulnerable(false);
-                        p.sendMessage(ChatColor.RED + "God off");
-                        e.getCurrentItem().removeEnchantment(Enchantment.MENDING);
-
-                        p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE , Note.natural(1, Note.Tone.G));
-                    }else {
-                        p.setInvulnerable(true);
-                        p.sendMessage(ChatColor.GREEN + "God on");
-                        e.getCurrentItem().addUnsafeEnchantment(Enchantment.MENDING, 1);
-
-                        p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE , Note.natural(1, Note.Tone.C));
-
-                    }
-                    break;
-
-                case FEATHER:
-                    if (p.getAllowFlight()){
-                        p.setAllowFlight(false);
-                        p.sendMessage(ChatColor.RED + "Fly off");
-                        e.getCurrentItem().removeEnchantment(Enchantment.MENDING);
-
-                       p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE , Note.natural(1, Note.Tone.G));
-                    }else {
-                        p.setAllowFlight(true);
-                        p.sendMessage(ChatColor.GREEN + "Fly on");
-                        e.getCurrentItem().addUnsafeEnchantment(Enchantment.MENDING, 1);
-
-                        p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE , Note.natural(1, Note.Tone.C));
-                    }
-                    break;
-                case GHAST_TEAR:
-                    if(plugin.invList.contains(p)){
-
-                        for(Player people : Bukkit.getOnlinePlayers()){
-                            people.showPlayer(plugin, p);
-                        }
-                        plugin.invList.remove(p);
-                        p.sendMessage(ChatColor.RED + "Vanish off");
-                        Bukkit.broadcastMessage(ChatColor.YELLOW + name + " joined the game");
-                        e.getCurrentItem().removeEnchantment(Enchantment.MENDING);
-
-                        p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE , Note.natural(1, Note.Tone.G));
-
-                        p.removeScoreboardTag("vanished");
-
-
-                    } else if(!plugin.invList.contains(p)){
-
-                        for(Player people : Bukkit.getOnlinePlayers()){
-                            people.hidePlayer(plugin, p);
-                        }
-                        plugin.invList.add(p);
-                        p.sendMessage(ChatColor.GREEN + "Vanish on");
-                        Bukkit.broadcastMessage(ChatColor.YELLOW + name + " left the game");
-                        e.getCurrentItem().addUnsafeEnchantment(Enchantment.MENDING, 1);
-
-                        p.playNote(p.getEyeLocation(), Instrument.XYLOPHONE , Note.natural(1, Note.Tone.C));
-
-                        p.addScoreboardTag("vanished");
-
-                    }
-                    break;
-
-                default:
-
-            }
-
-            e.setCancelled(true);
         }
     }
 }
